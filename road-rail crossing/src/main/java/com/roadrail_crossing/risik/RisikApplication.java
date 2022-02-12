7582fcf90473cf4663824fa365fb6d665e2222b2
markoercegovac <57296136+markoercegovac@users.noreply.github.com>
2022-02-12 21:19:50 +0100

    Initial structure

---
+++ road-rail crossing/src/main/java/com/roadrail_crossing/risik/RisikApplication.java
@@ -0,0 +1,13 @@
+package com.roadrail_crossing.risik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisikApplication.class, args);
+	}
+
+}
---
+++ road-rail crossing/src/test/java/com/roadrail_crossing/risik/RisikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadrail_crossing.risik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
